49ae26dfa36d22e2fd567fba8ad23b8777a406c1
Ellie Shin <ellie_shin@apple.com>
2022-10-18 17:02:24 -0700

    Add test for package APIs in chain

--- test/Sema/package-attr-decl-access-diags.swift
+++ test/Sema/package-attr-decl-access-diags.swift
@@ -4,6 +4,7 @@
 // RUN: %target-swift-frontend -emit-module %t/Countries.swift -module-name A -emit-module-path %t/Countries.swiftmodule
 // RUN: %target-swift-frontend -typecheck -verify -verify-ignore-unknown %t/CountryPicker-without-package-import.swift -I %t
 // RUN: %target-swift-frontend -typecheck -package-modules Countries -verify -verify-ignore-unknown %t/CountryPicker-with-package-import.swift -I %t
+// RUN: %target-swift-frontend -typecheck -package-modules Countries -verify -verify-ignore-unknown %t/CountryPicker-with-package-import-pass.swift -I %t
 
 
 // BEGIN Countries.swift
@@ -28,23 +29,49 @@ public struct Spain {
 // BEGIN CountryPicker-without-package-import.swift
 import Countries
 
-func showCountries() {
+func showSpain() -> Spain {
+  let s = Spain()
+  s.showCities() // expected-error{{cannot find 'Spain' in scope}}
+  return s
+}
+
+func showPortugal() {
   let p = Portugal()
   let eu = p.region
   let lisbon = p.city() // expected-error{{'city' is  inaccessible due to '@package' protection level}}
   print(eu, lisbon)
 
-  Spain().showCities() // expected-error{{cannot find 'Spain' in scope}}
 }
 
 // BEGIN CountryPicker-with-package-import.swift
 import Countries // -package-modules Countries is passed
 
-func showCountries() {
+func showSpain() -> Spain { // expected-error{{cannot use struct 'Spain' here; it is a package type imported from 'Countries'}}
+    let s = Spain()
+    s.showCities()
+    return s
+}
+
+func showPortugal() {
   let p = Portugal()
   let eu = p.region
   let lisbon = p.city()
   print(eu, lisbon)
-  Spain().showCities()
 }
 
+// BEGIN CountryPicker-with-package-import-pass.swift
+import Countries // -package-modules Countries is passed
+
+@package
+func showSpain() -> Spain { //'Spain' a package type imported from 'Countries'
+    let s = Spain()
+    s.showCities()
+    return s
+}
+
+func showPortugal() {
+  let p = Portugal()
+  let eu = p.region
+  let lisbon = p.city()
+  print(eu, lisbon)
+}
